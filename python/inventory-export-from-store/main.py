eab72470c1a26ae8313ccbe14be14b453837a131
Khaled Hasan <74279062+khaled-aiva@users.noreply.github.com>
2024-10-16 12:25:52 +0600

    updated shopify graphql request to get the status code and also added error handling logic

--- python/inventory-export-from-store/main.py
+++ python/inventory-export-from-store/main.py
@@ -16,7 +16,7 @@ def get_store_creds():
     store_version = os.getenv('SHOPIFY_API_VERSION')
 
     if not store_name or not store_token or not store_version:
-        raise ValueError("Shopify store credentials are missing in the .env file")
+        raise ValueError("Shopify store credentials are missing")
 
     return {
         'store_name': store_name,
@@ -31,22 +31,35 @@ def get_date_time_now():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 def shopify_gql_request(store, query):
-    headers = {
-        "Content-Type": "application/graphql",
-        "X-Shopify-Access-Token": store['store_token']
-    }
-    url = f"https://{store['store_name']}.myshopify.com/admin/api/{store['version']}/graphql.json"
-    response = requests.post(url, data=query, headers=headers)
-    return response.json()
+    try:
+        headers = {
+            "Content-Type": "application/graphql",
+            "X-Shopify-Access-Token": store['store_token']
+        }
+        url = f"https://{store['store_name']}.myshopify.com/admin/api/{store['version']}/graphql.json"
+        response = requests.post(url, data=query, headers=headers)
+        
+        return {
+            "status": response.status_code,
+            "data": response.json()
+        }
+    except Exception as e:
+        print(f"ERROR--- Shopify GQL Request ---ERROR => {str(e)}")
+        
+        return {
+            "status": 500,
+            "data": None
+        }
 
 def export_variants_inventory_to_csv():
     try:
         print(f"---***--- Inventory CSV generation task running ---***---")
 
         public_folder = "./public/"
-        exported_file_name = f"inventory_variants.csv"
+        exported_file_name = f"inventory_variants_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
         final_path_file = f"{public_folder}{exported_file_name}"
-
+        variant_count = 0
+        
         # CSV writing setup
         with open(final_path_file, mode='w', newline='') as file:
             writer = csv.writer(file)
@@ -63,8 +76,6 @@ def export_variants_inventory_to_csv():
                 'started': get_date_time_now(),
                 'ended': None
             }
-
-            variant_count = 0
             
             # Shopify API request cost and restore rate | please adjust according to the desired store
             request_cost = 150
@@ -73,7 +84,7 @@ def export_variants_inventory_to_csv():
             while is_continue:
                 query = f"""
                 {{
-                    productVariants(first: 200, {f'after: "{cursor}", ' if cursor else ''}) {{
+                    productVariants(first: 1, {f'after: "{cursor}", ' if cursor else ''}) {{
                         pageInfo {{
                             hasNextPage
                         }}
@@ -97,16 +108,22 @@ def export_variants_inventory_to_csv():
                 """
 
                 store_creds = get_store_creds()
-                shopify_response = shopify_gql_request(store_creds, query)
+                shopify_response_data = shopify_gql_request(store_creds, query)
+                status = shopify_response_data["status"]
+                shopify_response = shopify_response_data["data"]
                 data = shopify_response.get('data')
                 extensions = shopify_response.get('extensions')
-                status = shopify_response.get('status')
                 gql_errors = shopify_response.get('errors')
                 
-                print(shopify_response)
-                print(data)
-                print(extensions)
-                print(status)
+                # print(shopify_response)
+                # print(data)
+                # print(extensions)
+                # print(status)
+                
+                if status in [500]:
+                    print(f"Error ==> Something went wrong. Couldn't retrieve data from the store. StatusCode: {500}")
+                    is_continue = False
+                    continue
 
                 if status in [429, 502]:
                     print(f"Error ==> Retrying in 5s... StatusCode: {status}")
@@ -120,9 +137,9 @@ def export_variants_inventory_to_csv():
                         is_continue = False
 
                     cursor = variants['edges'][-1]['cursor']
-                    variant_count += len(variants['edges'])
 
                     for edge in variants['edges']:
+                        variant_count += 1
                         node = edge['node']
                         writer.writerow([
                             node['sku'],
@@ -132,9 +149,9 @@ def export_variants_inventory_to_csv():
                             f"https://{store_creds['store_name']}.myshopify.com/admin/products/{node['product']['legacyResourceId']}/variants/{node['legacyResourceId']}"
                         ])
 
-                    progress['total_variant_count'] += variant_count
+                    progress['total_variant_count'] = variant_count
                 else:
-                    sleep(5)
+                    sleep(1)
 
                 if extensions and extensions['cost']:
                     request_cost = extensions['cost']['requestedQueryCost']
@@ -149,7 +166,8 @@ def export_variants_inventory_to_csv():
             progress['completed'] = True
             progress['ended'] = get_date_time_now()
 
-        print(progress)
+        print(f'Total Variant Count ====> {progress['total_variant_count']}')
+        # print(progress)
         print(f"---*DONE*--- Inventory CSV export done---*DONE*---")
     except Exception as e:
         print(f"ERROR--- export_inventory_of_variants_to_csv ---ERROR => {str(e)}")
